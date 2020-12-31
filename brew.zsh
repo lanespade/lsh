dd5181e9b758469d2b23395f39bb4718d2087a78
Lane Spade <5313372+lanespade@users.noreply.github.com>
2020-12-31 13:54:07 -0800

    Updated brew URL

--- brew.zsh
+++ brew.zsh
@@ -2,7 +2,7 @@
 set -Eeuxo pipefail
 
 echo 'Installing homebrew'
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'Installing brews'
 brew reinstall fzf
